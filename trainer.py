a23a78560922cd87d9dd271dc86a8af0cc9a0238
Robert Bayer <robo.bayer@gmail.com>
2021-03-21 17:14:13 +0100

    step argument removed

--- trainer.py
+++ trainer.py
@@ -36,7 +36,6 @@ parser.add_argument('--epochs', default=1000, type=int, metavar='N', help='numbe
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N', help='manual epoch number (useful on restarts)')
 parser.add_argument('-b', '--batch-size', default=128, type=int, metavar='N',help='mini-batch size (default: 256), this is the total batch size of all GPUs on the current node when using Data Parallel or Distributed Data Parallel')
 parser.add_argument('--lr', '--learning-rate', default=0.01, type=float, metavar='LR', help='initial learning rate', dest='lr')
-parser.add_argument("--milestones", type=none_or_str, default='100,150,200,250', help="Scheduler milestones")
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum')
 parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,metavar='W', help='weight decay (default: 1e-4)', dest='weight_decay')
 parser.add_argument('-p', '--print-freq', default=10, type=int, metavar='N', help='print frequency (default: 10)')
